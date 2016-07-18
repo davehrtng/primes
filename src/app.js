01a32bdd36e1f9128191dba56bf8d3135c6dcc07
davehrtng <david.e.harting@gmail.com>
2016-07-18 19:58:28 -0400

    first primitive and incorrect implementation.

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const primes = require('./primes.js');
+console.log(primes);
+console.log(primes.listUpTo(1));
+console.log(primes.listUpTo(2));
---
+++ src/primes.js
@@ -0,0 +1,10 @@
+/** Defines utilities for working with primes */
+
+/**
+ * List all prime numbers up to and including the passed in variable.
+ */
+function listPrimesUpTo(upperBound) {
+  return [1, 2, 3, 5, 7, 11, 13];
+}
+
+exports.listUpTo = listPrimesUpTo;
